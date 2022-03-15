e78e116b0d42a705b4c7750a4bc07745a8193ca5
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-03-15 13:35:22 +0100

    Add test order

--- src/fp/util/test/Test.java
+++ src/fp/util/test/Test.java
@@ -8,4 +8,6 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Test {
+
+    int value() default 0;
 }
--- src/fp/util/test/UnitTest.java
+++ src/fp/util/test/UnitTest.java
@@ -3,6 +3,7 @@ package fp.util.test;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public abstract class UnitTest {
 
@@ -11,6 +12,7 @@ public abstract class UnitTest {
 
         Arrays.stream(this.getClass().getMethods())
                 .filter(method -> method.isAnnotationPresent(Test.class))
+                .sorted(Comparator.comparingInt(method -> method.getAnnotation(Test.class).value()))
                 .forEachOrdered(method -> this.testMethod(method, results));
 
         print("\n\n");
@@ -32,7 +34,7 @@ public abstract class UnitTest {
     }
 
     private void testMethod(Method method, TestResults results) {
-        print("\n\n");
+        print("\n");
         printSeparator();
         print("Test #" + method.getName() + ":");
         printSeparator();
