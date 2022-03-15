d006bf3ee205837021f1800ee7c9c21ebbc770ad
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-03-15 12:31:43 +0100

    Create unit test utils

---
+++ src/fp/util/test/UnitTest.java
@@ -0,0 +1,48 @@
+package fp.util.test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public abstract class UnitTest {
+
+    protected void printSeparator() {
+        print("=====================================");
+    }
+
+    protected void print(String s) {
+        System.out.println(s);
+    }
+
+    protected void print(Object o) {
+        System.out.println(o);
+    }
+
+    public void init() {
+        int successful = 0;
+        int exceptions = 0;
+
+        for (Method method : this.getClass().getMethods()) {
+            if (!method.isAnnotationPresent(Test.class))
+                continue;
+
+            print("\n\n");
+            printSeparator();
+            print("Test " + method.getName().replace("_", " ") + ":");
+            printSeparator();
+
+            try {
+                method.invoke(this);
+                successful++;
+            } catch (InvocationTargetException t) {
+                print("Catched " + t.getTargetException());
+                exceptions++;
+            } catch (IllegalAccessException ignored) {
+            }
+        }
+
+        print("\n\n");
+        printSeparator();
+        print("Finished running tests. " + successful + " successful tests and " + exceptions + " exceptions catched.");
+        printSeparator();
+    }
+}
