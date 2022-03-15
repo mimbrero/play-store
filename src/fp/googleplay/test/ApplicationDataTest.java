441fa2b0ddf723ebe79974edd148e835ed6cbb8a
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-03-15 17:42:59 +0100

    Use the new Assertions

--- src/fp/googleplay/test/ApplicationDataTest.java
+++ src/fp/googleplay/test/ApplicationDataTest.java
@@ -9,20 +9,9 @@ import fp.util.test.UnitTest;
 import java.time.Duration;
 import java.time.LocalDateTime;
 
-import static fp.util.Preconditions.assertThrows;
-import static fp.util.Preconditions.checkState;
+import static fp.util.test.Assertions.*;
 
 public class ApplicationDataTest extends UnitTest {
-
-    private final ApplicationData MOCK; // Mock de un ApplicationData para usar en los tests de métodos
-
-    public ApplicationDataTest() {
-        MOCK = new ApplicationData(
-                "Discord", AppCategory.COMMUNICATION, 4.7f, 900_000, "200M", 1_000_000_000,
-                0, LocalDateTime.of(2021, 12, 29, 0, 0), "v2.6",
-                "8.0", false
-        );
-    }
 
     //
     // Tests constructor 1
@@ -40,29 +29,33 @@ public class ApplicationDataTest extends UnitTest {
         print(data);
     }
 
-    // Prueba con nombre vacío
+    // Prueba con name vacío
     @Test(1)
     public void constructor1_EmptyName_ThenIllegalArgumentException() {
         String name = "";
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
                 () -> new ApplicationData(name, AppCategory.COMMUNICATION, 4.7f, 900_000, "200M",
                         1_000_000_000, 0, LocalDateTime.now(), "v2.6", "8.0",
-                        false)
+                        false),
+                "el constructor debería lanzar un IllegalArgumentException porque name está vacío"
         );
 
         print("Se ha lanzado la excepción " + exception);
     }
 
-    // Prueba con el parámetro actualización en el futuro (1/1/3000)
+    // Prueba con el parámetro lastUpdated en el futuro (1/1/3000)
     @Test(2)
     public void constructor1_FutureUpdate_ThenIllegalArgumentException() {
         LocalDateTime dateTime = LocalDateTime.of(3000, 1, 1, 0, 0);
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
                 () -> new ApplicationData("Discord", AppCategory.COMMUNICATION, 4.7f, 900_000,
                         "200M", 1_000_000_000, 0, dateTime, "v2.6", "8.0",
-                        false)
+                        false),
+                "el constructor debería lanzar un IllegalArgumentException porque lastUpdated es en el futuro"
         );
 
         print("Se ha lanzado la excepción " + exception);
@@ -74,9 +67,11 @@ public class ApplicationDataTest extends UnitTest {
         int installs = 0;
         int reviews = 500;
 
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
                 () -> new ApplicationData("Discord", AppCategory.COMMUNICATION, 4.7f, reviews, "200M",
-                        installs, 0, LocalDateTime.now(), "v2.6", "8.0", false)
+                        installs, 0, LocalDateTime.now(), "v2.6", "8.0", false),
+                "el constructor debería lanzar un IllegalArgumentException porque no puede haber valoraciones si tiene 0 descargas"
         );
 
         print("Se ha lanzado la excepción " + exception);
@@ -90,9 +85,9 @@ public class ApplicationDataTest extends UnitTest {
     public void constructor2_AllFine() {
         ApplicationData data = new ApplicationData("Discord", AppCategory.COMMUNICATION, "200M", 0, "v2.6", "8.0", false);
 
-        checkState(data.getRating() == 0);
-        checkState(data.getReviews() == 0);
-        checkState(data.getInstalls() == 0);
+        assertThat(data.getRating() == 0, "el constructor 2 debería poner rating en 0");
+        assertThat(data.getReviews() == 0, "el constructor 2 debería poner reviews en 0");
+        assertThat(data.getInstalls() == 0, "el constructor 2 debería poner installs en 0");
 
         print(data);
     }
@@ -101,12 +96,18 @@ public class ApplicationDataTest extends UnitTest {
     // Tests de métodos
     //
 
+    // Mock de un ApplicationData para usar en los tests de métodos
+    private final ApplicationData MOCK = new ApplicationData(
+            "Discord", AppCategory.COMMUNICATION, 4.7f, 900_000, "200M", 1_000_000_000,
+            0, LocalDateTime.of(2021, 12, 29, 0, 0), "v2.6",
+            "8.0", false
+    );
+
     // Prueba del método #type(). Saldrá FREE, porque MOCK tiene precio 0
     @Test(5)
     public void type_PriceIsZero_FREE() {
         ApplicationData data = MOCK;
-        checkState(data.getType() == AppType.FREE);
-
+        assertThat(data.getType() == AppType.FREE, "#getType() debería devolver FREE porque el precio es 0");
         print("Tipo = " + data.getType() + " (el precio es " + data.getPrice() + ").");
     }
 
@@ -116,7 +117,7 @@ public class ApplicationDataTest extends UnitTest {
         ApplicationData data = MOCK;
         LocalDateTime oneDayAfter = LocalDateTime.of(2021, 12, 30, 0, 0);
 
-        checkState(data.getTimeSinceLastUpdate(oneDayAfter).equals(Duration.ofDays(1)));
+        assertThat(data.getTimeSinceLastUpdate(oneDayAfter).equals(Duration.ofDays(1)), "el tiempo desde la última actualización debería de ser de 1 día");
         print("Tiempo desde la última actualización = " + data.getTimeSinceLastUpdate(oneDayAfter).toDays() + " días");
     }
 
