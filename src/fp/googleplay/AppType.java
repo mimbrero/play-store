4ff5f2863e6117c4a4e64d9b1a9d5e6864d34060
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-03-15 11:37:02 +0100

    Remove (now) unnecessary representation fields and logic

--- src/fp/googleplay/AppType.java
+++ src/fp/googleplay/AppType.java
@@ -1,35 +1,5 @@
 package fp.googleplay;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public enum AppType {
-
-    FREE("Free"),
-    PAID("Paid");
-
-    private final String representation;
-
-    AppType(String representation) {
-        this.representation = representation;
-    }
-
-    public String getRepresentation() {
-        return representation;
-    }
-
-    //
-    // Static access from representation to enum type:
-    //
-
-    private static final Map<String, AppType> REPRESENTATIONS = new HashMap<>();
-
-    static {
-        Arrays.stream(values()).forEach(value -> REPRESENTATIONS.put(value.getRepresentation(), value));
-    }
-
-    public static AppType parse(String toParse) {
-        return REPRESENTATIONS.get(toParse);
-    }
+    FREE, PAID;
 }
