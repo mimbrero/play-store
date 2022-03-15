0038cf776c7860da2108bb983441bc7d22686b1c
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-03-15 11:28:25 +0100

    Create AppCategory.java

---
+++ src/fp/googleplay/AppCategory.java
@@ -0,0 +1,14 @@
+package fp.googleplay;
+
+public enum AppCategory {
+    ART_AND_DESIGN,
+    AUTO_AND_VEHICLES,
+    BEAUTY,
+    BOOKS_AND_REFERENCE,
+    BUSINESS,
+    COMICS,
+    COMMUNICATION,
+    DATING,
+    EDUCATION,
+    ENTERTAINMENT;
+}
