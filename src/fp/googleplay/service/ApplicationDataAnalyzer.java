bd3b7b21e85da76c479f51c669f3be76aee908f8
Alberto Mimbrero <mimbrero.alberto@gmail.com>
2022-04-18 12:26:44 +0200

    Create ApplicationDataAnalyzer.java

---
+++ src/fp/googleplay/service/ApplicationDataAnalyzer.java
@@ -0,0 +1,18 @@
+package fp.googleplay.service;
+
+import fp.googleplay.ApplicationData;
+
+import java.util.Collection;
+
+// "analyzer" and not service as it stores the data to work with.
+public interface ApplicationDataAnalyzer {
+
+    Integer getDataSize();
+
+    void add(ApplicationData data);
+
+    void add(Collection<ApplicationData> data);
+
+    void remove(ApplicationData data);
+
+}
